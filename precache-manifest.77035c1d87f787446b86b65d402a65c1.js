caba13e62f43528ff0f9016fde47272a83606a3c
StanislavKozlovsky <staskozlovski506@gmail.com>
2019-05-16 19:15:05 +0300

    Updates

---
+++ precache-manifest.77035c1d87f787446b86b65d402a65c1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f79c55f3de788ab967f9308f66c2797",
+    "url": "/crypto-tracker/index.html"
+  },
+  {
+    "revision": "324baeb1de48c93976f1",
+    "url": "/crypto-tracker/static/css/main.a5824f4f.chunk.css"
+  },
+  {
+    "revision": "0ee377d2a049cbd2986d",
+    "url": "/crypto-tracker/static/js/2.bd1fa328.chunk.js"
+  },
+  {
+    "revision": "324baeb1de48c93976f1",
+    "url": "/crypto-tracker/static/js/main.750bc75c.chunk.js"
+  },
+  {
+    "revision": "99876c2a1b80c4ad3b72",
+    "url": "/crypto-tracker/static/js/runtime~main.62c4ffe5.js"
+  },
+  {
+    "revision": "a899a5866d6acff5522fd20fddf89ece",
+    "url": "/crypto-tracker/static/media/ATOM.a899a586.png"
+  },
+  {
+    "revision": "13adc83f24e3ff63a6eb3932b202eaa7",
+    "url": "/crypto-tracker/static/media/MKR.13adc83f.png"
+  },
+  {
+    "revision": "2d0f3c99f780b467e2074814d6f7ee45",
+    "url": "/crypto-tracker/static/media/github.2d0f3c99.svg"
+  },
+  {
+    "revision": "335ad49f8c1917250b4bd0c2c4201055",
+    "url": "/crypto-tracker/static/media/linkedin.335ad49f.svg"
+  }
+]);
